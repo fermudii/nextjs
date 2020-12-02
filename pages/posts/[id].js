97a15b2dcddafe640ad3a271429f9d91c8d091f0
fermudii <fernando.mudii@gmail.com>
2020-12-01 22:53:54 -0600

    prueba 1

---
+++ pages/posts/[id].js
@@ -0,0 +1,49 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+        <meta name="description" content="This is my login" />
+        <meta
+          property="og:description"
+          content="Esta es la descripcion estática"
+        />
+        <meta
+          property="og:image"
+          content="https://www.akawsports.com/wp-content/uploads/2018/01/Adidas-Logo-PNG-Image.png"
+        />
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXL}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  //Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
